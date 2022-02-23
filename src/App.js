fe3c3cc84a658492ac130fd589c80a06ce6e5e7d
dev-yoga <spin_shoppe@icloud.com>
2022-02-23 15:49:33 -0800

    Basic. List, display list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const poses = [
+    {
+      name: 'Corpse Pose',
+      sanskrit: 'Savasana',
+      pose_type: 'Supine',
+      url: 'https://www.yogajournal.com/poses/corpse-pose-2/',
+      objectID: 1
+    },
+    {
+      name: 'Monkey Pose',
+      sanskrit: 'Hanumanasana',
+      pose_type: 'Seated',
+      url: 'https://www.yogajournal.com/poses/monkey-pose/',
+      objectID: 2
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Yoga Poses</h1>
+      <Search />
+      <hr />
+      <List list={poses} />
     </div>
   );
-}
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.name}</a>
+    </span>
+    <span>{props.item.sanskrit}</span>
+    <span>{props.item.pose_type}</span>
+  </li>
+);
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={handleChange} />
+    <p>
+      Searching for <strong>{searchTerm}</strong>.
+    </p>
+  </div>
+  );
+};
 
 export default App;
